e5018253bc9f1fb8fc8ddda63f6b2009944d1460
jkjk807 <limjingkai123@gmail.com>
2022-12-19 18:59:17 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'doit';
+  title = 'DOIT';
+  counter: number = 0;
+  task="";
+  tasks: string []=[];
+  randomTask="";
+  max: number = 0;
+  removed=0;
+
+
+  addTask() {
+    this.tasks.push(this.task);
+  }
+  random(){
+   // this.randomTask=this.tasks[Math.random() * (this.tasks.length)];
+   this.max = Math.floor(this.tasks.length);
+   this.removed=Math.floor(Math.random() * (this.max  + 1));
+   this.randomTask=this.tasks[this.removed]
+   this.tasks.splice(this.removed,1)
+
+  }
 }
